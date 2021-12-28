6f0e5f45570ee453d1f83b0c82f44d7d81df976a
yangy <yangy@DESKTOP-PV86UN0>
2021-12-29 06:27:30 +0800

    small change.

--- ProjectCorpus/src/environment/EnvSetUp.java
+++ ProjectCorpus/src/environment/EnvSetUp.java
@@ -20,20 +20,9 @@ public class EnvSetUp {
 	public static final String user_home = System.getProperty("user.home");
 	public static final String witness = user_home + "/YYXWitness";
 	public static final String data = user_home + "/YYXData";
+	public static final String test = user_home + "/YYXTest";
 	
 	public static void main(String[] args) {
-		File witness_dir = new File(witness);
-		if (witness_dir.exists()) {
-			FileUtil.DeleteFile(witness_dir);
-		}
-		witness_dir.mkdirs();
-		
-		File data_dir = new File(data);
-		if (data_dir.exists()) {
-			FileUtil.DeleteFile(data_dir);
-		}
-		data_dir.mkdirs();
-		
 		JsonParser parser = new JsonParser();
 		JsonObject object = null;
 		try {
@@ -42,44 +31,11 @@ public class EnvSetUp {
 			e.printStackTrace();
 			System.exit(1);
 		}
-		{
-			JsonArray array = object.get("witness").getAsJsonArray();
-			ArrayList<File> wit_handles = new ArrayList<File>();
-			int as = array.size();
-			for (int a=0;a<as;a++) {
-				JsonElement ele = array.get(a);
-				String name = ele.getAsString();
-				File f = new File("all_projects" + "/" + name);
-				wit_handles.add(f);
-			}
-			for (File f : wit_handles) {
-				try {
-					FileUtil.CopyDirUnderSpecificDir(f.getAbsolutePath(), witness_dir.getAbsolutePath());
-				} catch (IOException e) {
-					e.printStackTrace();
-					System.exit(1);
-				}
-			}
-		}
-		{
-			JsonArray array = object.get("data").getAsJsonArray();
-			ArrayList<File> data_handles = new ArrayList<File>();
-			int as = array.size();
-			for (int a=0;a<as;a++) {
-				JsonElement ele = array.get(a);
-				String name = ele.getAsString();
-				File f = new File("all_projects" + "/" + name);
-				data_handles.add(f);
-			}
-			for (File f : data_handles) {
-				try {
-					FileUtil.CopyDirUnderSpecificDir(f.getAbsolutePath(), data_dir.getAbsolutePath());
-				} catch (IOException e) {
-					e.printStackTrace();
-					System.exit(1);
-				}
-			}
-		}
+		
+		PrepareDir(object, "witness", witness);
+		PrepareDir(object, "data", data);
+		PrepareDir(object, "test", test);
+		
 		String ast_meta = user_home + "/AST_Metas";
 		File dir = new File(ast_meta);
 		if (!dir.exists()) {
@@ -87,6 +43,32 @@ public class EnvSetUp {
 		}
 		FileUtil.CopyFile(new File("config.json"), new File(ast_meta + "/" + "config.json"));
 		System.out.println("Environment has been set up successfully.");
+	}
+	
+	private static void PrepareDir(JsonObject object, String dir_hint, String dir) {
+		File witness_dir = new File(dir);
+		if (witness_dir.exists()) {
+			FileUtil.DeleteFile(witness_dir);
+		}
+		witness_dir.mkdirs();
+		
+		JsonArray array = object.get(dir_hint).getAsJsonArray();
+		ArrayList<File> wit_handles = new ArrayList<File>();
+		int as = array.size();
+		for (int a=0;a<as;a++) {
+			JsonElement ele = array.get(a);
+			String name = ele.getAsString();
+			File f = new File("all_projects" + "/" + name);
+			wit_handles.add(f);
+		}
+		for (File f : wit_handles) {
+			try {
+				FileUtil.CopyDirUnderSpecificDir(f.getAbsolutePath(), witness_dir.getAbsolutePath());
+			} catch (IOException e) {
+				e.printStackTrace();
+				System.exit(1);
+			}
+		}
 	}
 	
 }
