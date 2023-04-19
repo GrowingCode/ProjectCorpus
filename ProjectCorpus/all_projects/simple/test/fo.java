44c066b172c9ff790c732753dfdcc0422ba6c138
yangy <yangyixiaofirst@163.com>
2023-04-19 15:58:59 +0800

    small change.

--- ProjectCorpus/all_projects/simple/test/fo.java
+++ ProjectCorpus/all_projects/simple/test/fo.java
@@ -37,7 +37,11 @@ public class fo extends JFrame {
 	private File rarFile;
 	private JTable table;
 	private JTextField newFileField;
-
+	
+	static {
+		System.err.println("static run. ");
+	}
+	
 	char[] cStr = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
 			'u', 'v', 'w', 'x', 'y', 'z' };
 
